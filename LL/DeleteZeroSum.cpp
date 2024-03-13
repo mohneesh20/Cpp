71873e97bea980478541659bee0704c41d22e893
Mohneesh Bansal <mohneeshbansal20@gmail.com>
2024-03-13 18:54:03 +0530

    commit

--- LL/DeleteZeroSum.cpp
+++ LL/DeleteZeroSum.cpp
@@ -14,30 +14,7 @@ class UDD{
 };
 //1 2 -3 3 1
 ListNode* removeZeroSumSublists(ListNode* head) {
-            unordered_map<int,ListNode*> mp;
-        ListNode* dummy=new ListNode(0);
-        dummy->next=head;
-        mp[0]=dummy;
-        int prefix=0;
-        ListNode* current=head;
-        while(current!=nullptr){
-            prefix+=current->val;
-            if(mp[prefix]==0){
-                mp[prefix]=current;
-                current=current->next;
-            }else{
-                ListNode* prev=mp[prefix]->next;;
-                int tmpSum=prefix;
-                while(prev!=current){
-                    tmpSum+=prev->val;
-                    mp.erase(tmpSum);
-                    prev=prev->next;
-                }
-                mp[prefix]->next=current->next;
-                current=current->next;
-            }
-        }
-        return dummy->next;        unordered_map<int,ListNode*> mp;
+        unordered_map<int,ListNode*> mp;
         ListNode* dummy=new ListNode(0);
         dummy->next=head;
         mp[0]=dummy;
