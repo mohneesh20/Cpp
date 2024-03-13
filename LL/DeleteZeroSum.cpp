d2fa9daada825316b77b6104216b3d8b5b8b709f
Mohneesh Bansal <mohneeshbansal20@gmail.com>
2024-03-13 18:52:48 +0530

    commit

--- LL/DeleteZeroSum.cpp
+++ LL/DeleteZeroSum.cpp
@@ -14,53 +14,53 @@ class UDD{
 };
 //1 2 -3 3 1
 ListNode* removeZeroSumSublists(ListNode* head) {
-    int cnt=0,count=0;
-    int prefix=0;
-    ListNode* head2=head,*tmp;
-    tmp=head;
-    unordered_map<int,UDD> mp;
-    while(tmp!=nullptr){
-        prefix+=tmp->val;
-        if(prefix==0){
-            count=cnt+1;
-            head2=tmp->next;
-        }
-        if(mp[prefix].a==-1){
-            mp[prefix].a=cnt;
-        }else{
-            mp[prefix].b=cnt;
-        }
-        cnt++;
-        tmp=tmp->next;        
-    }
-    if(prefix==0) {
-        return nullptr;
-    }
-    unordered_map<int,int> mp2;
-    for(int i=count;i<cnt;i++){
-        if(mp[i].a!=-1&&mp[i].b!=-1){
-            mp2[mp[i].a]=mp[i].b;
-        }
-    }
-    tmp=head2;
-    while(tmp&&tmp->next!=nullptr){
-        int skip=mp2[count];
-        if(skip==0){
-            tmp=tmp->next;
-            count++;
-        }else{
-            ListNode* tmp2=tmp;
-            while(count<skip){
-                tmp2=tmp2->next;
-                count++;
+            unordered_map<int,ListNode*> mp;
+        ListNode* dummy=new ListNode(0);
+        dummy->next=head;
+        mp[0]=dummy;
+        int prefix=0;
+        ListNode* current=head;
+        while(current!=nullptr){
+            prefix+=current->val;
+            if(mp[prefix]==0){
+                mp[prefix]=current;
+                current=current->next;
+            }else{
+                ListNode* prev=mp[prefix]->next;;
+                int tmpSum=prefix;
+                while(prev!=current){
+                    tmpSum+=prev->val;
+                    mp.erase(tmpSum);
+                    prev=prev->next;
+                }
+                mp[prefix]->next=current->next;
+                current=current->next;
             }
-            tmp->next=tmp2->next;
-            count++;
-            tmp=tmp->next;
         }
-    }
-
-    return head2;
+        return dummy->next;        unordered_map<int,ListNode*> mp;
+        ListNode* dummy=new ListNode(0);
+        dummy->next=head;
+        mp[0]=dummy;
+        int prefix=0;
+        ListNode* current=head;
+        while(current!=nullptr){
+            prefix+=current->val;
+            if(mp[prefix]==0){
+                mp[prefix]=current;
+                current=current->next;
+            }else{
+                ListNode* prev=mp[prefix]->next;;
+                int tmpSum=prefix;
+                while(prev!=current){
+                    tmpSum+=prev->val;
+                    mp.erase(tmpSum);
+                    prev=prev->next;
+                }
+                mp[prefix]->next=current->next;
+                current=current->next;
+            }
+        }
+        return dummy->next;
 }
 int main(){
     ListNode* head=nullptr,*tmp,*prev=nullptr;
